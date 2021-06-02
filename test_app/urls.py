de74a3d6d439e11b279126b11082aa3f945c5294
DianaReviako <dianarevako8@gmail.com>
2021-06-02 23:27:55 +0300

    Created a user registration form with the appropriate template

--- test_app/urls.py
+++ test_app/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import index_view, HomeView
+from .views import index_view, HomeView, RegistrationView
+
+
 urlpatterns = [
-    path('', HomeView.as_view(), name='index'),
+    path('', HomeView.as_view(), name='home'),
+    path('registration', RegistrationView.as_view(), name='registration'),
+    path('add_test', index_view, name='add_test')
 ]
--- test_app/views.py
+++ test_app/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render
-from django.views.generic.list import ListView
-from .models import Test, Question, Answer, UserAnswer
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView
+from .models import Test
+from .forms import RegisterUser
+
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+
+class RegistrationView(CreateView):
+    """User registration"""
+    template_name = 'registration.html'
+    form_class = RegisterUser
+    success_url = 'add_test'
 
 
 def index_view(request):
+    """Temporary stub function"""
     test = Test.objects.get(pk=1)
     return render(request, 'add_test.html', context={'test': test})
 
 
-class HomeView(ListView):
-    template_name = 'add_test.html'
-    model = Test
 
 
 
